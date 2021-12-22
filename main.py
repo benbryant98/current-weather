7699f9f4d36ab22555b48362ae13dbb6431e287c
benbryant98 <96531732+benbryant98@users.noreply.github.com>
2021-12-22 09:37:10 -0500

    Update main.py
    Adjusted base_url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ API_Key = '7ee41b1d6cc366853ce2341e256f4b73'
 
 city = input("Enter a city: ")
 
-base_url = "api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
+base_url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="
 
 weather_data = requests.get(base_url).json()
 
