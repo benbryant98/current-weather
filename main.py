efd37a099069c5fd78dd54f9cfb6e9c28134bd0e
benbryant98 <96531732+benbryant98@users.noreply.github.com>
2021-12-22 09:25:09 -0500

    Create main.py
    Current weather in inputted city

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from pprint import pprint
+
+API_Key = '7ee41b1d6cc366853ce2341e256f4b73'
+
+city = input("Enter a city: ")
+
+base_url = "api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
+
+weather_data = requests.get(base_url).json()
+
+pprint(weather_data)
