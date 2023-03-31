9b68453c5920e203dd8d825cd7ea345537ac330e
Ben <bryantbc98@gmail.com>
2023-03-31 11:48:12 -0400

    added five day forecast api functionality, algorithm dynamically creates elements based on response data

--- assets/script.js
+++ assets/script.js
@@ -17,9 +17,9 @@ $( document ).ready(function() {
 
     // converts city name string into coordinates using openweathermap geolocation API
     var getCoordinates = function (cityName) {
-        let apiURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=b87e30ca575aaba2c00121e487cdcd6c';
+        let apiCoord = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=b87e30ca575aaba2c00121e487cdcd6c';
 
-        fetch(apiURL)
+        fetch(apiCoord)
             .then(function(response) {
                 if (response.ok) {
                     response.json().then(function (data) {
@@ -36,9 +36,9 @@ $( document ).ready(function() {
     };
 
     var getCurrentWeather = function () {
-        let apiURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&appid=b87e30ca575aaba2c00121e487cdcd6c';
+        let apiCurrent = 'https://api.openweathermap.org/data/2.5/weather?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&appid=b87e30ca575aaba2c00121e487cdcd6c';
 
-        fetch(apiURL)
+        fetch(apiCurrent)
             .then(function (response) {
                 if (response.ok) {
                     response.json().then(function(data) {
@@ -48,16 +48,19 @@ $( document ).ready(function() {
                         let cityHeader = $('<h3>');
                         cityHeader.text(cityName);
                         currentWeather.append(cityHeader);
+                        let iconData = $('<img>')
                         let tempData = $('<p>');
                         let windData = $('<p>');
                         let humidityData = $('<p>');
 
                         // set text to data value from weather API response
-                        tempData.text('Temperature: ' + data.main.temp +  '\u00b0F');
-                        windData.text('Wind: ' + data.wind.speed + 'mph');
+                        iconData.attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png')
+                        tempData.text('Temperature: ' + data.main.temp.toString().slice(0,2) +  '\u00b0F');
+                        windData.text('Wind: ' + data.wind.speed.toString().slice(0,2) + 'mph');
                         humidityData.text('Humidity: ' + data.main.humidity + '%');
 
                         // append elements to div
+                        currentWeather.append(iconData);
                         currentWeather.append(tempData);
                         currentWeather.append(windData);
                         currentWeather.append(humidityData);
@@ -65,5 +68,55 @@ $( document ).ready(function() {
                     });
                 };
             });
+    };
+    var getFiveDay = function () {
+        let apiFiveDay = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&appid=b87e30ca575aaba2c00121e487cdcd6c';
+
+        fetch(apiFiveDay)
+            .then(function(response) {
+                if(response.ok) {
+                    response.json().then(function(data) {
+                        console.log(data);
+
+                        let fiveDayHeader = $('<h3>');
+                        fiveDayHeader.text('5 Day Forecast:');
+                        fiveDay.append(fiveDayHeader);
+                        
+                        var fiveDayData = [];
+                        for(i=0;i<5;i++) {
+                            fiveDayData[i] = {
+                                date: data.list[i].dt_txt.slice(0,10),
+                                icon: data.list[i].weather[0].icon,
+                                temp: data.list[i].main.temp.toString().slice(0,2),
+                                wind: data.list[i].wind.speed.toString().slice(0,2),
+                                humidity: data.list[i].main.humidity
+                            };
+
+                            console.log(fiveDayData[i]);
+                            let dayDiv = $('<div>');
+                            dayDiv.addClass('day-div');
+
+                            let fiveDate = $('<p>');
+                            let fiveIcon = $('<img>');
+                            let fiveTemp = $('<p>');
+                            let fiveWind = $('<p>');
+                            let fiveHumidity = $('<p>');
+
+                            fiveDate.text(fiveDayData[i].date);
+                            fiveIcon.attr('src','http://openweathermap.org/img/w/' + fiveDayData[i].icon + '.png');
+                            fiveTemp.text('Temp: ' + fiveDayData[i].temp + '\u00b0F');
+                            fiveWind.text('Wind: ' + fiveDayData[i].wind + 'mph');
+                            fiveHumidity.text('Humidity: ' + fiveDayData[i].humidity + '%');
+
+                            dayDiv.append(fiveDate);
+                            dayDiv.append(fiveIcon);
+                            dayDiv.append(fiveTemp);
+                            dayDiv.append(fiveWind);
+                            dayDiv.append(fiveHumidity);
+                            weatherCard.append(dayDiv);
+                        };
+                    })
+                }
+            })
     }
 });
